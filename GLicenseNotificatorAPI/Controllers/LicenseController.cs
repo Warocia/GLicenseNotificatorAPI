da612cb4d951b85020404efe5c7ab85e79a1358a
Warocia <rauhala.antti@proton.me>
2023-01-29 08:00:23 +0200

    License can now be editet

--- GLicenseNotificatorAPI/Controllers/LicenseController.cs
+++ GLicenseNotificatorAPI/Controllers/LicenseController.cs
@@ -2,7 +2,9 @@ using GLicenseNotificatorAPI.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 using System.Security.Claims;
+
 
 namespace GLicenseNotificatorAPI.Controllers
 {
@@ -20,18 +22,111 @@ namespace GLicenseNotificatorAPI.Controllers
             _configuration = configuration;
         }
 
-        [HttpGet(Name = "GetLicense")]
-        public IEnumerable<License> Get()
+        [HttpPost(Name = "UpdateUserLicenseList")]
+        public ActionResult UpdateUserLicenseList(List<Model.License> licenses)
         {
-            string userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            LicenceUser? user;
+            DataContext? db;
 
-            var db = new Model.DataContext(_configuration);
-          
-            var user = db.Users.Include(u => u.Licenses).FirstOrDefault(u => u.UserName == userName);
+            try
+            {
+                string? userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            if (user == null) {return new List<License>();}
+                if (userName == null)
+                {
+                    return Unauthorized();
+                }
 
-            return user.Licenses.ToList();
+                db = new Model.DataContext(_configuration);
+                user = db.Users.Include(u => u.Licenses).FirstOrDefault(u => u.UserName == userName);
+
+                if (user == null)
+                {
+                    return Unauthorized();
+                } 
+            }
+            catch (Exception)
+            {
+                return Unauthorized();
+            }
+
+            try
+            {
+                var dbLicenses = user.Licenses.ToList();
+
+                foreach (var dbLicense in dbLicenses)
+                {
+                    var license = licenses.FirstOrDefault(l => l.Id == dbLicense.Id);
+
+                    if (license == null)
+                    {
+                        // dbLicense does not exist in licenses, so delete from db
+                        user.Licenses.Remove(dbLicense);
+                    }
+                    else
+                    {
+                        // dbLicense exists in licenses, so update if needed
+                        if (dbLicense.LicenseNumber != license.LicenseNumber ||
+                            dbLicense.IsValidUtc != license.IsValidUtc ||
+                            dbLicense.NotificationSent != license.NotificationSent)
+                        {
+                            dbLicense.LicenseNumber = license.LicenseNumber;
+                            dbLicense.IsValidUtc = license.IsValidUtc;
+                            dbLicense.NotificationSent = license.NotificationSent;
+                        }
+
+                        //Remove that we don't try add these
+                        licenses.Remove(license);
+                    }
+                }
+
+                // Check for new licenses that were not in the db
+                foreach (var license in licenses)
+                {
+                    db.Entry(license).State = EntityState.Added;
+                    user.Licenses.Add(license);
+                }
+
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            return Ok();
+        }
+
+        [HttpGet(Name = "GetUserLicenseList")]
+        public ActionResult<IEnumerable<Model.License>> GetUserLicenseList()
+        {
+            LicenceUser? user;
+            try
+            {
+                string? userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (userName == null)
+                {
+                    return Unauthorized();
+                }
+                var db = new Model.DataContext(_configuration);
+                user = db.Users.Include(u => u.Licenses).FirstOrDefault(u => u.UserName == userName);
+            }
+            catch (Exception)
+            {
+                return Unauthorized();
+            }
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            try
+            {
+                return user.Licenses.ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
